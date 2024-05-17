e7d09203a764205e093f6acb739a160e7802d06f
AriRainbow <mariel.williams@atlasschool.com>
2024-05-17 18:37:23 +0000

    Hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,12 +3,12 @@
 /**
  * main - Entry point
  *
- * Description: This program prints exactly, followed by a new line.
+ * Description: This program prints input exactly, followed by a new line.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts(""Programming is like building a multilingual puzzle");
 	return (0);
 }
