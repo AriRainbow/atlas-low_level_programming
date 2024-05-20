9a725b0f354efeca8d0f393e488903a6fb8f6d53
AriRainbow <mariel.williams@atlasschool.com>
2024-05-20 00:12:56 +0000

    alphABET

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		putchar(alph);
+	}
+
+	for (alph = 'A'; alph <= 'Z'; alph++)
+	{
+		putchar(alph);
+	}
+
+	putchar('\n');
+
+	return 0;
+}
