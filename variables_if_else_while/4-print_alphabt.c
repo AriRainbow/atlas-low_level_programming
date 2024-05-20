370048f1e99183f1512857ef2c55a0960ca7c225
AriRainbow <mariel.williams@atlasschool.com>
2024-05-20 01:07:22 +0000

    When I was having that alphabet soup, I never thought that it would pay off

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+	
+		if (alph != 'q' && alph != 'e')
+
+		{
+
+		putchar(alph);
+
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
