9b76f84da1187ce1b1d0cfeeb3f0eb02b9fef4e4
AriRainbow <mariel.williams@atlasschool.com>
2024-05-21 02:29:49 +0000

    _putchar

--- functions_nested_loops/_putchar.c
+++ functions_nested_loops/_putchar.c
@@ -1,1 +1,19 @@
-Not empty
+#include "main.h"
+#include <unistd.h> /* header file for the 'write' system call */
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: 1 (success). -1 (error)
+ *
+ */
+
+int _putchar(char c)
+	/* Takes character c and returns as integer */
+{
+	return write(1, &c, 1);
+	/** '1': file descriptor for stdout
+	 *  '&c': address of the character to write
+	 *  '1': number of bytes to write (single character = 1) */ 
+}
---
+++ functions_nested_loops/main.h
@@ -0,0 +1,2 @@
+int _putchar(char c);
+/* Function declaration for _putchar */
