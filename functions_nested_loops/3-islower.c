4dc43816d7636e1e9d98a72e88d95a5a21a652d3
AriRainbow <mariel.williams@atlasschool.com>
2024-05-21 04:28:01 +0000

    islower

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -4,8 +4,7 @@
  * _islower - Checks for lowercase character.
  * @c: The character to check.
  *
- * Return: 1 if c is lowercase
- * Return: 0 otherwise
+ * Return: 1 if c is lowercase, 0 otherwise
  */
 
 int _islower(int c)
