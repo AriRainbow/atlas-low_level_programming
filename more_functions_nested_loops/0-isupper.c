678b1374cf785a7d26c36c1ca6e76f3f422bc66c
AriRainbow <mariel.williams@atlasschool.com>
2024-06-01 17:42:26 +0000

    isupper

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+#include <ctype.h>
 
 /**
  * _isupper - Function checks for uppercase character
@@ -10,4 +10,12 @@
 
 int _isupper(int c)
 {
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
