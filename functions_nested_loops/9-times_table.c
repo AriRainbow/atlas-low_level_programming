7b5631fc7051f00fd5cbb7b90f835acc41995cd4
AriRainbow <mariel.williams@atlasschool.com>
2024-06-01 02:45:09 +0000

    Learn your times table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,1 +1,16 @@
 #include "main.h"
+#include <stdio.h>
+
+/**
+ * times_table - Prints the 9 times table starting with 0
+ */
+
+void times_table(void)
+{
+	int row, col, product;
+
+	for (row = 0; row <= 9; row++)
+	{
+		for (col = 0; col <= 9; col++)
+		{
+			product = row * col;
