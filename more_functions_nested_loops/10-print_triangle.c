8226d5be71ad1819087ff9260aeb4d7ec521ff5f
AriRainbow <mariel.williams@atlasschool.com>
2024-06-02 02:30:10 +0000

    Triangles

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,1 +1,12 @@
 #include "main.h"
+
+/**
+ * print_triangle - Function prints a triangle, followed by new line
+ * @size: Size of triangle
+ * @#: Character to print the triangle
+ *
+ */
+
+void print_triangle(int size)
+{
+}
