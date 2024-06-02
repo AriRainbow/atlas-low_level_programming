e7e3d041389ec5f940b427728802514ebd6c4970
AriRainbow <mariel.williams@atlasschool.com>
2024-06-02 00:40:04 +0000

    You are so much sunshine in every square inch

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,1 +1,29 @@
 #include "main.h"
+
+/**
+ * print_square - Function prints a square, followed by new line
+ * @size: Size of square
+ * @#: Character used to print the square
+ *
+ */
+
+void print_square(int size)
+{
+	int x, y;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y < size; y++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
