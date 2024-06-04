e22cf9668efcc6bb9b7a721cbbab8c2434d63b38
AriRainbow <mariel.williams@atlasschool.com>
2024-06-04 04:06:48 +0000

    I am a kind of paranoid in reverse. I suspect people of plotting to make me happy

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -13,5 +13,19 @@
 
 void reverse_array(int *a, int n)
 {
+	int start = 0; /* start from beginning of array */
+	int end = n - 1; /* start from end of array */
+	int temp; /* helps with swapping elements */
 
+	while (start < end)
+	{
+		/* swap symmetrically around midpoint */
+		temp = a[start];
+		a[start] = a[end];
+		a[end] = temp;
+
+		/* move pointers toward midpoint */
+		start++;
+		end--;
+	}
 }
