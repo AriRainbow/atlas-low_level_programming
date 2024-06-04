88761df84a8275fd6c5cf69509a482dbcffeb4c0
AriRainbow <mariel.williams@atlasschool.com>
2024-06-04 03:15:53 +0000

    strncpy

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -17,5 +17,17 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
+	int i;
 
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	for (; i < n; i++)
+	{
+		dest[i] = '\0'; /* remaining character null bytes */
+	}
+
+	return (dest);
 }
