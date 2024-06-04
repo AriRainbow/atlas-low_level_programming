2d78a6853b4d36c176a71929753ad1f64f06b74c
AriRainbow <mariel.williams@atlasschool.com>
2024-06-04 03:52:04 +0000

    I am a kind of paranoid in reverse. I suspect people of plotting to make me happy

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,1 +1,17 @@
 #include "main.h"
+
+/**
+ * reverse_array - Function reverses content of array of integers
+ *
+ * @a: Pointer to array of integers
+ * @n: Number elements in array
+ *
+ * Description: Reverses by swapping elements symetrically
+ * around its midpoint
+ *
+ */
+
+void reverse_array(int *a, int n)
+{
+
+}
