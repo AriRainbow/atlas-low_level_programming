26063109457616c2d9fc8244dce9449147db78c7
AriRainbow <mariel.williams@atlasschool.com>
2024-06-04 02:48:08 +0000

    strncat

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,1 +1,39 @@
 #include "main.h"
+
+/**
+ * _strncat - Function concatenates two strings
+ *
+ * @dest: Pointer to destination string
+ * @src: Pointer to source string
+ * @n: Max number of bytes from src to concatenate
+ *
+ * Description:
+ *
+ * Return: dest
+ *
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int dlength = 0;
+	int i = 0;
+
+	/* find length of destination */
+	while (dest[dlength] != '\0')
+	{
+		dlength++;
+	}
+
+	/* append at most n bytes from src to dest */
+	while (src[i] != '\0' && i < n)
+	{
+		dest[dlength + i] = src[i];
+		i++;
+	}
+
+	/* add null terminator */
+	dest[dlength + i] = '\0';
+
+	/* return pointer */
+	return (dest);
+}
