2f63c36c95c4b8be82d7e1de2c2ba5c444921baf
AriRainbow <mariel.williams@atlasschool.com>
2024-06-04 00:37:15 +0000

    I do not fear computers. I fear the lack of them

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,1 +1,21 @@
 #include "main.h"
+
+/**
+ * _puts - Function prints a string, followed by new line,
+ * to stout
+ *
+ * @str: Pointer to string
+ *
+ * Description: Uses helper function _putchar
+ *
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
