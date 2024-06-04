8581f9658415803b7aba0b1c9459a559969055d7
AriRainbow <mariel.williams@atlasschool.com>
2024-06-04 00:10:30 +0000

    This report, by its very length, defends itself against the risk of being read

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,1 +1,23 @@
 #include "main.h"
+
+/**
+ * _strlen - Function returns length of a string
+ *
+ * @s: Pointer to string
+ *
+ * Description: Iterates through string until null terminator
+ *
+ * Return: Length of string
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	return (length);
+}
