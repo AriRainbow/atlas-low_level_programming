447347731e5812f512829ac5828a80ccd6f4cdc5
AriRainbow <mariel.williams@atlasschool.com>
2024-06-04 02:26:08 +0000

    strcat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,1 +1,36 @@
 #include "main.h"
+
+/**
+ * _strcat - Function concatenates two strings
+ *
+ * @dest: Destination string
+ * @src: Source string
+ *
+ * Description:
+ *
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int dlength = 0; /* destination length */
+	int n = 0; /* counter */
+
+	/* find destination length */
+	while (dest[dlength] != '\0')
+	{
+		dlength++;
+	}
+
+	/* append source to destination */
+	while (src[n] != '\0')
+	{
+		dest[dlength + n] = src[n];
+		n++;
+	}
+
+	/* New null terminator */
+	dest[dlength + n] = '\0';
+
+	/* return pointer */
+	return (dest);
+}
