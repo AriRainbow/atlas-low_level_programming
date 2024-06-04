58c674eab07dc423b4a1f9df81d7a501a703c98e
AriRainbow <mariel.williams@atlasschool.com>
2024-06-04 04:45:11 +0000

    Expect the best. Prepare for the worst. Capitalize on what comes

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,1 +1,46 @@
 #include "main.h"
+
+/**
+ * cap_string - Function capitalizes all words of string
+ *
+ * @str: String to modify
+ *
+ * Return: str
+ *
+ */
+
+char *cap_string(char *str)
+{
+	int n = 0;
+	int x;
+
+	/* list of separators */
+	char separators[] = " \t\n,;.!?\"(){}";
+
+	/* cap first character if lowercase letter */
+	if (str[n] >= 'a' && str[n] <= 'z')
+	{
+		str[n] -= 32;
+	}
+
+	/* iterate string */
+	while (str[n] != '\0')
+	{
+		/* check for separator */
+		for (x = 0; separators[x] != '\0'; x++)
+		{
+			if (str[n] == separators[x])
+			{
+				/* cap next if lowercase */
+				if (str[n + 1] >= 'a' && str[n + 1] <= 'z')
+				{
+					str[n + 1] -= 32;
+				}
+				break; /* exit loop once separator found */
+			}
+		}
+		n++;
+	}
+
+	return (str);
+}
