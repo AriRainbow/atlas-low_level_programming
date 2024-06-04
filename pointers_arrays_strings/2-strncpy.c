ca82a5677330a69fed27e8d0e883debceab4844a
AriRainbow <mariel.williams@atlasschool.com>
2024-06-04 03:17:27 +0000

    strncpy

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -7,8 +7,8 @@
  * @src: Pointer to source string
  * @n: Max bytes to copy from src
  *
- * Description: Copies at most n bytes from src to dest. No null 
- * byte in first n bytes of src, results in dest will not be 
+ * Description: Copies at most n bytes from src to dest. No null
+ * byte in first n bytes of src, results in dest will not be
  * null terminated.
  *
  * Return: dest
