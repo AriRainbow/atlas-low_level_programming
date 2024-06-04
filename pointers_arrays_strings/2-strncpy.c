8c2d0c2cd32bfe8b4ddf13dc561f248f67a043b1
AriRainbow <mariel.williams@atlasschool.com>
2024-06-04 03:02:24 +0000

    strncpy

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,1 +1,21 @@
 #include "main.h"
+
+/**
+ * _strncpy - Function copies a string
+ *
+ * @dest: Pointer to destination string
+ * @src: Pointer to source string
+ * @n: Max bytes to copy from src
+ *
+ * Description: Copies at most n bytes from src to dest. No null 
+ * byte in first n bytes of src, results in dest will not be 
+ * null terminated.
+ *
+ * Return: dest
+ *
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+
+}
