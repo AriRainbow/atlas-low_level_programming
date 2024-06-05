229e62944890ec89d2a14793de20e347be793673
AriRainbow <mariel.williams@atlasschool.com>
2024-06-05 04:43:51 +0000

    Half the lies they tell about me aren't true

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,1 +1,27 @@
 #include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - Function prints every other character
+ *
+ * @str: String to iterate
+ *
+ * Description: Starts at first char, followed by new line
+ *
+ */
+
+void puts2(char *str)
+{
+	int n; /* loop counter */
+
+	/* iterate string */
+	for (n = 0; str[n] != '\0'; n++) /* loop until end */
+	{
+		if (n % 2 == 0) /* check if index even */
+		{
+			putchar(str[n]); /* print current even index */
+		}
+	}
+
+	putchar('\n'); /* new line */
+}
