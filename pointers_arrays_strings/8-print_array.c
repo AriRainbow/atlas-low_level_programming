96df85c90901831b77a1351b525ecdfb6f476f33
AriRainbow <mariel.williams@atlasschool.com>
2024-06-07 01:53:35 +0000

    Arrays are not pointers

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,1 +1,17 @@
 #include "main.h"
+
+/**
+ * print_array - Function prints n elements of 
+ * array of integers, followed by new line.
+ *
+ * @a:
+ * @n:
+ *
+ * Description:
+ *
+ */
+
+void print_array(int *a, int n)
+{
+	n
+}
