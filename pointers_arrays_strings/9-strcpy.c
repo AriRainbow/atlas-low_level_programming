651f59335bbebf847d665a132e9963074850620e
AriRainbow <mariel.williams@atlasschool.com>
2024-06-07 03:18:50 +0000

    strcpy

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,1 +1,19 @@
 #include "main.h"
+
+/**
+ * _strcpy - Function copies str pointed by src to 
+ * buffer pointed by dest.
+ *
+ * @dest: Pointer to destination buffer
+ * @src: Pointer to source string
+ *
+ * Description:
+ *
+ * Return: dest
+ *
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	n
+}
