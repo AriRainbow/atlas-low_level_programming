637331cfc7b4748a982866e8ddb27baf955ea544
AriRainbow <mariel.williams@atlasschool.com>
2024-06-09 08:00:32 +0000

    3.

---
+++ recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * factorial - Function returns factorial of given number
+ *
+ * @n: number to calc factorial
+ *
+ * Description: Factorial of 0 is 1
+ *
+ * Return: Factorial of n, if n < 0 returns -1
+ *
+ */
+
+int factorial(int n)
+{
+	n
+}
