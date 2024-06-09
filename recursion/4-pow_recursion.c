b90e1fe2b99e9143f3674a16a8184f38971b458a
AriRainbow <mariel.williams@atlasschool.com>
2024-06-09 21:15:48 +0000

    Once an idea has taken hold of the brain it's almost impossible to eradicate

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Function returns value of x raised to power of y
+ *
+ * @x:
+ * @y:
+ *
+ * Description:
+ *
+ * Return:
+ *
+ */
+
+int _pow_recursion(int x, int y)
+{
+	n
+}
