9ccf2228b21aa6bf7ab25b24775a45649a51dedc
AriRainbow <mariel.williams@atlasschool.com>
2024-06-09 21:51:34 +0000

    Once an idea has taken hold of the brain it's almost impossible to eradicate mandatory

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -3,16 +3,29 @@
 /**
  * _pow_recursion - Function returns value of x raised to power of y
  *
- * @x:
- * @y:
+ * @x: base value
+ * @y: exponent value
  *
- * Description:
+ * Description: x^y = x * x^(y-1)
  *
- * Return:
+ * Return: Result of x^y, -1 if y < 0
  *
  */
 
 int _pow_recursion(int x, int y)
 {
-	n
+	if (y < 0)
+	{
+		return (-1);
+	}
+	
+	else if (y == 0) /* base case */
+	{
+		return (1);
+	}
+
+	else /* recursive case */
+	{
+		return (x * _pow_recursion(x, y-1));
+	}
 }
