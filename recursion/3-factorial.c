aef9d8fd670c576d6e3e0905dbb807b7892c4203
AriRainbow <mariel.williams@atlasschool.com>
2024-06-09 21:08:36 +0000

    You mustn't be afraid to dream a little bigger, darling

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -6,6 +6,7 @@
  * @n: number to calc factorial
  *
  * Description: Factorial of 0 is 1
+ * Factorial of n defined as n! = n (n-1) * (n-2) * ...
  *
  * Return: Factorial of n, if n < 0 returns -1
  *
@@ -13,5 +14,18 @@
 
 int factorial(int n)
 {
-	n
+	if (n < 0)
+	{
+		return (-1);
+	}
+
+	else if (n == 0 || n == 1) /* base case */
+	{
+		return (1);
+	}
+
+	else
+	{
+		return n * factorial(n - 1); /* recursive case */
+	}
 }
