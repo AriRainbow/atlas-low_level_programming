fda5d17a57cd0754a1ecc5e005fbabc53be540ac
AriRainbow <mariel.williams@atlasschool.com>
2024-06-09 06:52:56 +0000

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,1 +1,22 @@
 #include "main.h"
+
+/**
+ * _puts_recursion - Function prints string, followed by new line
+ *
+ * @s: string to be printed
+ *
+ * Description: 
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0') /* base case */
+	{
+		_putchar('\n');
+		return; /* stop recursion */
+	}
+
+	_putchar(*s); /* print current */
+	_puts_recursion(s + 1); /* recursive call */
+}
