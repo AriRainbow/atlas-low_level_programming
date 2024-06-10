bb84960f47a79a377340106a74de8472b3e8938d
AriRainbow <mariel.williams@atlasschool.com>
2024-06-10 01:40:31 +0000

    Your subconscious is looking for the dreamer

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -35,11 +35,11 @@ int _sqrt_recursion(int n)
 
 int _sqrt(int n, int i)
 {
-        if (i * i > n)
-                return (-1);
+	if (i * i > n)
+        	return (-1);
 
-        if (i * i == n)
-                return (i); /*natural square root */
+	if (i * i == n)
+        	return (i); /*natural square root */
 
-        return (_sqrt(n, i + 1)); /* continue next number */
+	return (_sqrt(n, i + 1)); /* continue next number */
 }
