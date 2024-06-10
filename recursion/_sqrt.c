c67aedaf94676c1da5feffd2beb50e081b22b30d
AriRainbow <mariel.williams@atlasschool.com>
2024-06-10 01:37:45 +0000

    Your subconscious is looking for the dreamer

--- recursion/_sqrt.c
+++ recursion/_sqrt.c
@@ -1,26 +1,5 @@
 #include "main.h"
 
-int _sqrt(int n, int i); /* helper function */
-
-/**
- * _sqrt_recursion - Function returns natural square root of number
- *
- * @n: Number to find square root of
- *
- * Description: Uses helper function _sqrt
- *
- * Return: Natural square root of n, or -1 if none
- *
- */
-
-int _sqrt_recursion(int n)
-{
-	if (n < 0)
-		return (-1);
-
-	return (_sqrt(n, 0)); /* start recursion with 0 */
-}
-
 /**
  * _sqrt - Helper function finds natural square root
  *
