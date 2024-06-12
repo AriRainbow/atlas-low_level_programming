21f4f20374ee3212ff804946f17b09c63c549cb5
AriRainbow <mariel.williams@atlasschool.com>
2024-06-12 23:00:34 +0000

    Neither irony nor sarcasm is argument

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: Argument counter
+ * @argv: Argument vector
+ *
+ * Return: Product of two numbers 0 (success), otherwise  1 (error)
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	return (0);
+}
