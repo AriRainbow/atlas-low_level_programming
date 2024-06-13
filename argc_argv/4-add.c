f4044386eafc3321c00f2bac3ff78b67d1cdc0a4
AriRainbow <mariel.williams@atlasschool.com>
2024-06-13 01:02:00 +0000

    To infinity and beyond

---
+++ argc_argv/4-add.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: Argument counter
+ * @argv: Argument vector
+ *
+ * Return: Sum of two numbers 0 (success), otherwise 1 (error)
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	return (0);
+}
