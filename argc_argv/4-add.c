f4f47fc0fa9622d485bd966907a6008a672012b0
AriRainbow <mariel.williams@atlasschool.com>
2024-06-13 01:49:17 +0000

    To infinity and beyond

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -8,6 +8,7 @@
  * @argv: Argument vector
  *
  * Description: If no number, print 0 followed by new line.
+ * If symbol, print error followed by new line.
  *
  * Return: Sum positive numbers 0 (success), otherwise 1 (error)
  *
@@ -15,5 +16,15 @@
 
 int main(int argc, char *argv[])
 {
+	int sum;
+	int a, b;
+
+	for (a = 1; a < argc; a++) /* outer loop, each argument */
+	{
+		for (b = 0; argv[a][b] != '\0'; b++)
+			/* inner loop, each character */
+		{
+			if()
+	}
 	return (0);
 }
