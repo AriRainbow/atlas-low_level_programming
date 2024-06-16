ca5f29d84df782009e277ca37238b13e20d309bb
AriRainbow <mariel.williams@atlasschool.com>
2024-06-16 03:02:42 +0000

    He who is not courageous enough to take risks will accomplish nothing in life

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -4,10 +4,12 @@
 /**
  * str_concat - Function concatenates two strings
  *
- * @s1:
- * @s2:
+ * @s1: String 1
+ * @s2: String 2
  *
- * Return:
+ * Description: If NULL is passed, treat as empty string
+ *
+ * Return: NULL on failure, otherwise pointer to new malloc
  *
  */
 
@@ -15,6 +17,20 @@ char *str_concat(char *s1, char *s2)
 {
 	char *concat_str;
 	unsigned int len_s1, len_s2; /* do I need entry point? */
-	unsigned int a; /* s1 counter */
-	unsigned int b; /* s2 counter */
+	unsigned int a; /* counter */
+	unsigned int b; /*  */
+
+	/* NULL as empty string */
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	/* calculate string lengths */
+	for (a = 0; s1[a] != '\0'; a++)
+		len_s1++;
+
+	for (a = 0; s2[a] != '\0'; a++)
+		len_s2++;
 }
