3b4c0223eb383e68a8d79daab6e4108ac5f765fd
AriRainbow <mariel.williams@atlasschool.com>
2024-06-16 02:17:56 +0000

    He who is not courageous enough to take risks will accomplish nothing in life

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - Function concatenates two strings
+ *
+ * @s1:
+ * @s2:
+ *
+ * Return:
+ *
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *concat_str;
+	unsigned int len_s1, len_s2; /* do I need entry point? */
+	unsigned int a; /* s1 counter */
+	unsigned int b; /* s2 counter */
+}
