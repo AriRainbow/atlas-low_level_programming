cd12bdd2e2ef4b684f2fdfdf57af816fcfc9388d
AriRainbow <mariel.williams@atlasschool.com>
2024-06-16 01:30:01 +0000

    The woman who has no imagination has no wings

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -22,4 +22,15 @@ char *_strdup(char *str)
 
 	for (length = 0; str[length] != '\0'; length++)
 		/* calculate input length */
+		; /* only */
+
+	dup_str = malloc((length + 1) * sizeof(char));
+	/* lenght + 1 for NULL terminator */
+	if (dup_str == NULL) /* insufficient mem */
+		return NULL;
+
+	for (n = 0; n <= length; n++)
+		dup_str[n] = str[n]; /* copy string */
+
+	return (dup_str);
 }
