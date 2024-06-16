58acaa0531f78db3e8139ed6b1f6a251c33215f6
AriRainbow <mariel.williams@atlasschool.com>
2024-06-16 03:45:44 +0000

    He who is not courageous enough to take risks will accomplish nothing in life

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -18,7 +18,7 @@ char *str_concat(char *s1, char *s2)
 	char *concat_str;
 	unsigned int len_s1, len_s2; /* do I need entry point? */
 	unsigned int a; /* counter */
-	unsigned int b; /*  */
+	unsigned int b; /* counter */
 
 	/* NULL as empty string */
 	if (s1 == NULL)
@@ -29,8 +29,26 @@ char *str_concat(char *s1, char *s2)
 
 	/* calculate string lengths */
 	for (a = 0; s1[a] != '\0'; a++)
-		len_s1++;
+		len_s1++; /* yes, need = 0 above? */
 
 	for (a = 0; s2[a] != '\0'; a++)
 		len_s2++;
+
+	/* malloc for concat_str */
+	concat_str = malloc((len_s1 + len_s2 + 1) * sizeof(char));
+	if (concat_str == NULL) /* insufficient memory */
+		return (NULL);
+
+	/* copy s1 to concat_str */
+	for (a = 0; s1[a] != '\0'; a++)
+		concat_str[a] = s1[a];
+
+	/* copy s2 to concat_str */
+	for (b = 0; s2[b] != '\0'; b++)
+		concat_str[b] = s2[b];
+
+	/* NULL terminated */
+	concat_str[a] = '\0';
+
+	return (concat_str);
 }
