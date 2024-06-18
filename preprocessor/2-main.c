ab5fed1684f1aa54223a9843e3fad09134e52422
AriRainbow <mariel.williams@atlasschool.com>
2024-06-18 07:03:50 +0000

    File name

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints name of file it was compiled from 
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
 int main(void)
 {
 	printf("%s\n", __FILE__);
