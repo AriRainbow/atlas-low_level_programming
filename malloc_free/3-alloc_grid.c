b9f74f86ee84d355893572656e2561405c026ce4
AriRainbow <mariel.williams@atlasschool.com>
2024-06-18 03:02:33 +0000

    If you even dream of beating me you'd better wake up and apologize

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -17,6 +17,35 @@
 
 int **alloc_grid(int width, int height)
 {
-	int
-}
+	int **grid;
+	int a, b;
 
+	/* check for invalid input */
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	/* malloc row pointers */
+	grid = malloc(height * sizeof(int *));
+	if (grid == NULL)
+		return (NULL);
+
+	/* initialize and malloc each row */
+	for (a = 0; a < height; a++)
+	{
+		grid[a] = malloc(width + sizeof(int));
+		if (grid[a] == NULL)
+		{
+			/* free rowa on failure */
+			for (b = 0; b < a; b++)
+				free(grid[b]);
+			free(grid);
+			return (NULL);
+		}
+
+		/* initialize elements */
+		for (b = 0; b < width; b++)
+			grid[a][b] = 0;
+	}
+
+	return (grid);
+}
