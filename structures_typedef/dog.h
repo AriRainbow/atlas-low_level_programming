3d40ce889801ea203567fb64c08bf10546ef146f
AriRainbow <mariel.williams@atlasschool.com>
2024-06-21 19:28:47 +0000

    Poppy

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Describes a dog
+ *
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner of dog
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
