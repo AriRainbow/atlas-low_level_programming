6a7e28e6ceb5cca5e5ca51ac70d65b5668d27ea3
AriRainbow <mariel.williams@atlasschool.com>
2024-06-21 22:59:30 +0000

    Outside of a dog, a book is a man's best friend. Inside of a dog it's too dark to read

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,8 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stddef.h>
+
 /**
  * struct dog - Describes a dog
  *
@@ -16,6 +18,8 @@ struct dog
 	float age;
 	char *owner;
 };
+
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 /* ptr allows the func to modify original struct dog instead of copy */
