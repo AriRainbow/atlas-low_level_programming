1eb69083812db295f0639367dbd87484cc840cbf
AriRainbow <mariel.williams@atlasschool.com>
2024-06-23 22:10:06 +0000

    If you spend too much time thinking about a thing, you'll never get it done

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,1 +1,23 @@
 #include "function_pointers.h"
+
+/**
+ * print_name - Function that prints a name
+ *
+ * @name: Name to print
+ * @f: Pointer to function used for printing the name
+ *
+ * Description: Function takes a name and a function pointer
+ * as arguments, and calls the function pointer with the 
+ * name as its argument.
+ *
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
+	f(name); /* calls func pointed to by f */
+	/* passing name as an argument */
+}
