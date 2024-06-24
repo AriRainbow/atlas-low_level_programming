154dba59ad4b38dd572771e3cbb67ee8fe1e7d3c
AriRainbow <mariel.williams@atlasschool.com>
2024-06-24 06:41:25 +0000

    A goal is not always meant to be reached, it often serves simply as something to aim at

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -10,8 +10,8 @@
  * Description: op is pointer to a string, represents an operator.
  * f is function pointer, func takes 2 int as argument and returns int.
  *
- * Return: Pointer to the function that corresponds to the 
- * operator given as parameter, or NULL if operator does not 
+ * Return: Pointer to the function that corresponds to the
+ * operator given as parameter, or NULL if operator does not
  * match any of the expected operators.
  *
  */
@@ -20,14 +20,14 @@ int (*get_op_func(char *s))(int, int)
 {
 	/* ops is an array of op_t structure */
 	op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i;
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+	};
+       	int i;
 
     i = 0; /* loop to find correct function */
     while (ops[i].op != NULL) /* stop loop at end of array */
