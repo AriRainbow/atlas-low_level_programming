6b6f0a7a039e33bf8941559644702f0297c09c01
AriRainbow <mariel.williams@atlasschool.com>
2024-07-07 08:08:08 +0000

    List length

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - Returns number of elements in linked list_t list.
+ * @h: Pointer to head of list.
+ *
+ * Description:
+ * Return: Number of elements in the list 
+ */
+size_t list_len(const list_t *h)
+{
+	size_t node_count = 0; /* initialize to zero */
+
+	while (h != NULL) /* traverse the list */
+	{
+		h = h->next; /* move to next node */
+		node_count++; /* counter */
+	}
+
+	return node_count; /* total number of nodes */
+}
