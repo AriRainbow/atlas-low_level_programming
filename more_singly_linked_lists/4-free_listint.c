afa89c9cb682e63014434ff6113727548580872b
AriRainbow <mariel.williams@atlasschool.com>
2024-07-08 21:19:28 +0000

    Free list

---
+++ more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint - Frees a listint_t list
+ * @head: Pointer to head of list
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp; /* temp pointer to hold current node */
+
+	while (head != NULL) /* loop until all nodes freed */
+	{
+		temp = head; /* save current node */
+		head = head->next; /* move to next node */
+		free (temp); /* free current node */
+	}
+}
