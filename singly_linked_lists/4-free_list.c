5c7ad53d75898b2a247c2f9cfeb83e126001ad57
AriRainbow <mariel.williams@atlasschool.com>
2024-07-08 03:32:39 +0000

    Free list

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,1 +1,19 @@
-#include
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - Frees a t_list list
+ * @head: Pointer to the head of the list
+ */
+void free_list(list_t *head)
+{
+	list_t *temp_node; /* temp pointer to hold node being freed */
+
+	while (head != NULL)
+	{
+		temp_node = head; /* save current head */
+		head = head->next; /* move to next node */
+		free(temp_node->str); /* free string of current node */
+		free(temp_node); /* free current node */
+	}
+}
