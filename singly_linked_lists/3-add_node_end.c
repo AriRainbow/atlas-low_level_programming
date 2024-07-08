ab42a28c6f1d7e42eae4703c653ea4e4cc5d2f28
AriRainbow <mariel.williams@atlasschool.com>
2024-07-08 03:10:49 +0000

    Add node at the end

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,1 +1,57 @@
-#include
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node_end - Adds a new node at end of list_t list
+ * @head: Double pointer to the head of the list
+ * @str: String to be added to new node
+ *
+ * Description:
+ * Return: Address of new element, or NULL if failed
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node; /* pointer to new node */
+	list_t *temp = *head; /* temp pointer to traverse list */
+	char *new_str; /* pointer to duplicated string */
+	unsigned int len = 0; /* length of new string */
+
+	/* duplicate string and check for success */
+	new_str = strdup(str);
+	if (new_str == NULL)
+		return (NULL);
+
+	/* calculate length of new string */
+	while (str[len])
+		len++;
+
+	/* allocate memory for new node */
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		free(new_str);
+		return (NULL);
+	}
+
+	/* set the new node's values */
+	new_node->str = new_str;
+	new_node->len = len;
+	new_node->next = NULL;
+
+	/* make new node head if list empty */
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+
+	/* traverse to end of list */
+	while (temp->next)
+		temp = temp->next;
+
+	/* add new node at end */
+	temp->next = new_node;
+
+	return (new_node); /* address of new element */
+}
