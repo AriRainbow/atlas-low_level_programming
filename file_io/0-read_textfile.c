77eac35f372c70396c769c0a9c267a1d012a1474
AriRainbow <mariel.williams@atlasschool.com>
2024-07-10 03:03:48 +0000

    betty

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -9,7 +9,7 @@
  * @filename: Name if the file to read
  * @letters: Number of letters it should read and print
  *
- * Description: File descriptors are integers that uniquely 
+ * Description: File descriptors are integers that uniquely
  * represent an open file within a process
  *
  * Return: Actual number of letters it could read and print,
@@ -41,10 +41,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (num_read == -1) /* check if read operation was successful */
 	{
 		free(buffer); /* free allocated buffer */
-		close (file_descriptor);
+		close(file_descriptor);
 		return (0);
 	}
-
 	/* write buffer to standard output */
 	num_written = write(STDOUT_FILENO, buffer, num_read);
 	if (num_written == -1 || num_written != num_read)
